6e76d014bc408499b887d3859d645d77d815a9d8
arthclement <arthclems@gmail.com>
2018-03-01 17:02:49 +0100

    Exercice 7 done

--- PHP/Exercice7/exercise.php
+++ PHP/Exercice7/exercise.php
@@ -1,1 +1,22 @@
 <?php
+
+function divide($dividende, $divisor) {
+    if($divisor == 0) {
+        throw new RuntimeException('Division by 0 is not allowed');
+        return;
+    }
+
+    return $dividende / $divisor;
+}
+
+function arrayDivide($arrayToDivide, $divisor) {
+    $arrayOfResults = [];
+    foreach($arrayToDivide as $dividende) {
+        try {
+            $arrayOfResults[] = $dividende / $divisor;
+        } catch (RuntimeException $e) {
+            return $arrayToDivide;
+        }
+    return $arrayOfResults;
+    }
+}
